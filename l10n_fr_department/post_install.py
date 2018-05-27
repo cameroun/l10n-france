46a34a5b41bd1ea4db0b9a75cc2899ab20f8395e
Clovis NZOUENDJOU <clovis@anybox.fr>
2018-05-28 00:18:37 +0200

    Fix coding errror

--- l10n_fr_department/post_install.py
+++ l10n_fr_department/post_install.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
--- l10n_fr_siret/__manifest__.py
+++ l10n_fr_siret/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # © 2011 Numérigraphe SARL.
 # © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
